b4da654ac057536e88b4687969ca6db71f2caca7
Sangam Khanal <sangamkhnl@gmail.com>
2019-09-30 21:24:54 +0545

    Lexer works for single token, Need to add loop

--- Lexer.cpp
+++ Lexer.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+//TODO: Create a function that takes in different parameters for different html tags and pushes all of them into the stack
+//TODO: Create a similar function to delete them from the stack
 Lexer::Lexer(string &inputstring)
 {
 	
@@ -47,8 +49,10 @@ void Lexer::evaluate_token()
 		case '<':
 			inc_pointer();
 			checkfirst();
+			break;
 		default:
 			showerror("Error in Evaluate_token");
+			break;
 	}
 }
 void Lexer::checkfirst()
@@ -58,28 +62,84 @@ void Lexer::checkfirst()
 		case 'h':
 			inc_pointer();
 			checkforh();
+			break;
 		case 't':
 			inc_pointer();
 			checkfort();
+			break;
 		case 'b':
 			inc_pointer();
 			checkforb();
+			break;
 		case 'p':
 			inc_pointer();
 			checkforp();
+			break;
 		case '/':
+			cout <<"Start deletionkfk, everything correct till now"<<endl;
+			break;
 			// Start deleting from the stack
 		default:
 			showerror("Error in the first letter");
+			break;
+	}
+}
+void Lexer::checkforp()
+{
+	if(inputstr[str_pointer]=='>')
+	{
+		cout << "P found "<< endl;
+		// Add p to the stack
+	}
+	else
+	{
+		showerror("P not found");
 	}
 }
 void Lexer::checkforb()
 {
 	switch(inputstr[str_pointer])
 	{
-		
+		case 'o':
+			inc_pointer();
+			checkforbody();
+			break;
+		case 'r':
+			inc_pointer();
+			checkforbr();
+			break;
+		default:
+			showerror("Error in checkforb");
+			break;
 	}
 }
+void Lexer::checkforbody()
+{
+	string temp = inputstr.substr(str_pointer,str_pointer+3);
+	inc_pointer(3);
+	if(temp.compare("dy>"))
+	{
+		cout << "Body Found"<< endl;
+	}
+	else
+	{
+		showerror("Body not found");
+	}	
+}
+void Lexer::checkforbr()
+{
+	if(inputstr[str_pointer]=='>')
+	{
+		cout << "Br found"<< endl;
+		//Push this to the stack
+	}
+	else
+	{
+		showerror("Br not found");
+	}
+	
+}
+
 void Lexer::checkfort()
 {
 	switch(inputstr[str_pointer])
@@ -87,8 +147,10 @@ void Lexer::checkfort()
 		case 'i':
 			inc_pointer();
 			checkfortitle();
+			break;
 		default:
 			showerror("Error in checkfort");
+			break;
 	}
 
 }
@@ -101,6 +163,11 @@ void Lexer::checkfortitle()
 		cout << "Title found"<< endl;
 		// Push title to the stack
 	}
+	else
+	{
+		showerror("Title Not found");
+	}
+	
 
 }
 void Lexer::checkforh()
@@ -119,11 +186,14 @@ void Lexer::checkforh()
 			case 'e':
 				inc_pointer();
 				checkforhead();
+				break;
 			case 't':
 				inc_pointer();
 				checkforhtml();
+				break;
 			default:
-				showerror(1);
+				showerror("Error in h");
+				break;
 
 		}
 		
@@ -135,9 +205,14 @@ void Lexer::checkforhtml()
 	inc_pointer(3);
 	if(temp.compare("ml>"))
 	{
-		cout << "Html Start"<< endl;
+		cout << "Html Starts"<< endl;
 		// push html to stack
 	}
+	else
+	{
+		showerror("Html not found");
+	}
+	
 }
 void Lexer:: checkforhead()
 {
@@ -148,6 +223,11 @@ void Lexer:: checkforhead()
 		cout << "Head Start" << endl;
 		// Push head to the stack
 	}
+	else
+	{
+		showerror("Head not found");
+	}
+	
 
 }
 void Lexer:: checkforh1()
